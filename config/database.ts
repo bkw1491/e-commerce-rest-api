a1b0e18da49e2304685239edbd962349d4a27086
Brandon <bkw1491@gmail.com>
2021-11-29 15:15:38 +0000

    setup connection pool to mysql database

--- config/database.ts
+++ config/database.ts
@@ -2,9 +2,9 @@ import mysql from 'mysql2';
 
 export const pool = mysql.createPool({
   connectionLimit: 100,
-  host: process.env["DB_HOST"],
-  port: process.env["DB_PORT"],
-  user: 'root',
-  password: '', 
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, 
   database: 'e-commerce'
 });
