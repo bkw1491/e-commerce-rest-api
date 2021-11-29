5599d524e57977f3880b757aa524a536278200fe
Brandon <bkw1491@gmail.com>
2021-11-29 15:53:57 +0000

    setup express server

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,7 @@
 namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: string;
+    PORT: number;
     DB_HOST: string,
     DB_USER : string
     DB_PASSWORD: string
---
+++ server.ts
@@ -0,0 +1,14 @@
+//resolve .env variables
+//?? move this somewhere else?
+import { resolve } from "path"
+import { config } from "dotenv"
+config({ path: resolve(__dirname, "./config/.env") })
+
+import express from 'express';
+
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`⚡[Server]: Listening On Port ${process.env.PORT}`)
+});
