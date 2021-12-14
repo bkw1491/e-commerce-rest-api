4e32b06c4653f61f8a90ec1d9cdb1b36f5c6e10d
Brandon <bkw1491@gmail.com>
2021-12-14 10:26:47 +0000

    add token expire time

--- middlewares/verify.ts
+++ middlewares/verify.ts
@@ -3,10 +3,17 @@ import { Request, Response, NextFunction } from 'express';
 
 export function verifyJWT(req: Request, res: Response, next: NextFunction) {
 
+  
+
   //return unauthorized if header not provided
   if(!req.headers.authorization) { res.sendStatus(401) }
+  //verify the token, returns extracted token if valid
+  const token = verify(req.headers['authorization']!)
   //send 401 if token verification fails
-  if(!verify(req.headers['authorization']!)) { res.sendStatus(401) }
+  if(!token) { res.sendStatus(401) }
+
+  console.log(token);
+  
   //next middleware
   next();
 }
