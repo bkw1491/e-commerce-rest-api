ab3ad57826425ebfb4766e23cd7095c4e16ba83b
Brandon <bkw1491@gmail.com>
2021-12-21 11:34:25 +0000

    add method to find user by id

--- models/user.model.ts
+++ models/user.model.ts
@@ -3,39 +3,56 @@ import query from '@config/database';
 import { IUser } from '@interfaces/IUser';
 import { hash } from '@utils/crypt';
 
+
 export const UserModel = {
   
   /**
-   * Finds a user. Returns null if not found 
+   * Finds a user by email. Returns null if 
+   * the user is not found 
    */
-  async findOne(email: string) : Promise<IUser> {
-  
+  async findOneByEmail(email: string) {
     //email is unique
-    const sql = `SELECT *
-                 FROM users
-                 WHERE email = $1
-                 LIMIT 1`;
+    const sql = `SELECT  *
+                 FROM    users
+                 WHERE   email = $1
+                 LIMIT   1`;
     //use prepared statement
     const result = await query<IUser>(sql, [email]);
     //always only 1 result
     return result[0];
   },
+
+
+  /**
+   * Finds a user by id. Returns null if
+   * the user is not found
+   */
+  async findOneById(id: number) {
+
+    const sql = `SELECT  *
+                 FROM    users
+                 WHERE   id = $1`;
+
+    const result = await query<IUser>(sql, [id]);
+
+    return result[0];
+  },
   
   
   /**
-   * Adds a new user. Returns the user_id
+   * Hashes the password then adds the new user 
+   * to the database. Returns the user_id
    */
-  async createOne(user: IUser) : Promise<number> {
+  async createOne(user: IUser) {
   
-    //call crypt util to hash the password
     const hashed = hash(user.password);
-    //db auto generates the user_id
-    const sql = `INSERT INTO users (email, password)
-                 VALUES ($1, $2)
-                 RETURNING *`;
-    //use prepared statement
+
+    const sql = `INSERT INTO   users (email, password)
+                 VALUES        ($1, $2)
+                 RETURNING     *`;
+ 
     const result = await query<IUser>(sql, [user.email, hashed]);
-    //return the id back to the user
+
     return result[0].id;
   }
 }
