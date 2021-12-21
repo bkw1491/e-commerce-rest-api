fb4481a4c498eee8003513a6fcaa15915fe540f6
Brandon <bkw1491@gmail.com>
2021-12-21 14:26:32 +0000

    rename to IOrderItem

--- interfaces/IOrder.ts
+++
@@ -1,5 +0,0 @@
-export interface IOrder {
-  id: number,
-  user_id: number,
-  total_cost: number
-}
---
+++ interfaces/IOrderItem.ts
@@ -0,0 +1,6 @@
+export interface IOrderItem {
+  id: number,
+  user_id: number,
+  product_id: number,
+  quantity: number
+}
