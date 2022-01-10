a417e803248296656d3ede3a8779b91becefb8d8
Brandon <bkw1491@gmail.com>
2022-01-10 15:33:32 +0000

    add confirm password requirement

--- interfaces/IUser.ts
+++ interfaces/IUser.ts
@@ -1,6 +1,6 @@
 export interface IUser {
-  id: number,
-  email: string,
+  id: number
+  email: string
   password: string
   admin: boolean
 }
--- schema/user.schema.ts
+++ schema/user.schema.ts
@@ -15,16 +15,21 @@ const user = object({
     string().
     min(8).
     max(255),
+  confirm:
+    string().
+    max(255)
 })
 
 
 export const UserSchema = {
 
   register: user.omit({id: true}).
-    refine(async input => !await UserModel.findOneByEmail(input.email),
-    {message: "email already in use"}),
+    refine(async input => { return !await UserModel.findOneByEmail(input.email) },
+    {message: "email in use"}).
+    refine(input => { return input.password === input.confirm },
+    {message: "passwords do not match"}),
 
-  auth: user.omit({id: true}).
+  auth: user.omit({id: true, confirm: true}).
     refine(async input => {
       //look for user with supplied email
       const user = await UserModel.findOneByEmail(input.email);
