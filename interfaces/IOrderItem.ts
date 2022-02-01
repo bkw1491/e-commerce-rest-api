1e10c738e96b9fb9d9584e56e012d2754952bd8c
Brandon <bkw1491@gmail.com>
2022-02-01 14:19:30 +0000

    fix issue where order_id was wrong type

--- interfaces/IOrderItem.ts
+++ interfaces/IOrderItem.ts
@@ -1,9 +1,8 @@
 import { IProduct } from "@interfaces/IProduct";
 
-//TODO this is the same as ICartItem, reference that instead?
 export interface IOrderItem extends Pick<IProduct, "name" | "descr" | "price"> {
   id: number,
-  order_id: number
+  order_id: string
   product_id: number,
   quantity: number,
 }
