46b6e3e2344d8aa36d53742cc72e4c7a176ad006
Brandon <bkw1491@gmail.com>
2022-02-03 14:26:02 +0000

    supply 'next' in args so express recognises as error handler

--- middlewares/error.ts
+++ middlewares/error.ts
@@ -1,12 +1,13 @@
 import log from '@utils/logger';
 
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { toResponse } from '@utils/response';
 
 
-export function errorHandler(err: any, req: Request, res: Response) {
+export function errorHandler(
+  err: any, _req: Request, res: Response, _next: NextFunction) {
   //log the error to the console
-  log(err.message);
+  log(`${err.name} (${err.message})`);
   //return error to user
-  res.status(500).send(toResponse("unexpected internal server error"));
+  res.status(500).send(toResponse("internal server error"));
 }
