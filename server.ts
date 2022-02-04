7d955a8a635fe6c9c1bf3703eb3773cc3c1da4eb
Brandon <bkw1491@gmail.com>
2022-02-04 15:07:40 +0000

    change origin

--- server.ts
+++ server.ts
@@ -22,7 +22,7 @@ app.use(cookieParser());
 //adds the Access-Control-Allow-Credentials header to req
 //TODO this will change for production
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'http://localhost:3000/', 
   credentials: true
 }));
 
