1dca9265c015d2f380c7e2b9947a7afb7f0aec64
Brandon <bkw1491@gmail.com>
2022-02-05 07:20:44 +0000

    change origin in cors

--- server.ts
+++ server.ts
@@ -1,30 +1,32 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import log from '@utils/logger';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import log from "@utils/logger";
 
-import { userRouter } from '@routes/user';
-import { cartRouter } from '@routes/cart';
-import { adminRouter } from '@routes/admin';
-import { shopRouter } from '@routes/shop';
-import { webhookRouter } from '@routes/webhook';
-import { errorHandler } from '@middlewares/error';
-import { docsRouter } from '@routes/docs';
+import { userRouter } from "@routes/user";
+import { cartRouter } from "@routes/cart";
+import { adminRouter } from "@routes/admin";
+import { shopRouter } from "@routes/shop";
+import { webhookRouter } from "@routes/webhook";
+import { errorHandler } from "@middlewares/error";
+import { docsRouter } from "@routes/docs";
 
 //initlaize express
 const app = express();
 
 //attackers can use this header to detect apps running Express
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 //need this since jwt stored in httpOnly cookie for now
 app.use(cookieParser());
 
 //adds the Access-Control-Allow-Credentials header to req
 //TODO this will change for production
-app.use(cors({
-  origin: 'http://localhost:3000/', 
-  credentials: true
-}));
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true
+	})
+);
 
 //expects raw format
 app.use("/webhook", webhookRouter);
@@ -41,5 +43,5 @@ app.use(errorHandler);
 
 //listen on port defined in .env file
 app.listen(process.env.PORT, () => {
-  log(`Listening On Port ${process.env.PORT}`);
+	log(`Listening On Port ${process.env.PORT}`);
 });
