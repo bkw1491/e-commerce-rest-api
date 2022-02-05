786d7f7600aceb128276c9d3c67da0622875e9cb
Brandon <bkw1491@gmail.com>
2022-02-05 08:02:36 +0000

    cleanups

--- middlewares/verify.ts
+++ middlewares/verify.ts
@@ -1,54 +1,51 @@
-import { toError } from '@utils/response';
-import { constructEvent } from '@utils/stripe';
-import { verify } from '@utils/token';
-import { Request, Response, NextFunction } from 'express';
-
+import { toError } from "@utils/response";
+import { constructEvent } from "@utils/stripe";
+import { verify } from "@utils/token";
+import { Request, Response, NextFunction } from "express";
 
 export function verifyJWT(protection: "admin" | "user") {
-
-  return (req: Request, res: Response, next: NextFunction) => {
-    //return unauthorized if auth cookie not provided
-    if(!req.cookies.auth) {
-      return res.status(401).send(toError("token not provided"))
-    }
-    //verify the token, returns extracted token if valid
-    const token = verify(req.cookies.auth!)
-    //send 401 if token verification fails
-    if(!token) {
-       return res.status(401).send(toError("token invalid")) 
-    }
-    //if the route requires admin and user not admin return 401
-    if(protection === "admin") {
-      if(!token.payload.admin) {
-        return res.status(401).send(toError("protected route"))
-      }
-    }
-    //atatch user_id to req body, makes life easier later on
-    req.body.user_id = token.payload.sub;
-    //move to next middleware
-    next();
-  }
+	return (req: Request, res: Response, next: NextFunction) => {
+		//return unauthorized if auth cookie not provided
+		if (!req.cookies.auth) {
+			return res.status(401).send(toError("token not provided"));
+		}
+		//verify the token, returns extracted token if valid
+		const token = verify(req.cookies.auth!);
+		//send 401 if token verification fails
+		if (!token) {
+			return res.status(401).send(toError("token invalid"));
+		}
+		//if the route requires admin and user not admin return 401
+		if (protection === "admin") {
+			if (!token.payload.admin) {
+				return res.status(401).send(toError("protected route"));
+			}
+		}
+		//atatch user_id to req body, makes life easier later on
+		req.body.user_id = token.payload.sub;
+		//move to next middleware
+		next();
+	};
 }
 
-
 export function verifyWebhook() {
-  //as per stripe docs, verifies req is coming from stripe
-  return ((req: Request, res: Response, next: NextFunction) => {
-    //get the stripe signature from req header
-    const signature = req.headers['stripe-signature'];
-    //if no signature, return unauthorized
-    if(!signature) { return res.status(401).send(toError("unauthorized")) }
-    //constructEvent will throw if invalid
-    try {
-      //attempt to verify the signature
-      req.body = constructEvent(req.body, signature as string);
-      //return to the router
-      next();
-    }
-
-    catch(err) {
-      //auth failed
-      res.status(401).send(toError("unauthorized"));
-    }
-  })
+	//as per stripe docs, verifies req is coming from stripe
+	return (req: Request, res: Response, next: NextFunction) => {
+		//get the stripe signature from req header
+		const signature = req.headers["stripe-signature"];
+		//if no signature, return unauthorized
+		if (!signature) {
+			return res.status(401).send(toError("unauthorized"));
+		}
+		//constructEvent will throw if invalid
+		try {
+			//attempt to verify the signature
+			req.body = constructEvent(req.body, signature as string);
+			//return to the router
+			next();
+		} catch (err) {
+			//auth failed
+			res.status(401).send(toError("unauthorized"));
+		}
+	};
 }
--- routes/user/auth.ts
+++ routes/user/auth.ts
@@ -1,24 +1,23 @@
-import express from 'express';
+import express from "express";
 
-import { Request, Response } from 'express';
-import { validate } from '@middlewares/validate';
-import { asyncHandler } from '@middlewares/async';
-import { UserSchema } from '@schemas/user';
-import { issue } from '@utils/token';
-import { toResponse } from '@utils/response';
-import { cookie } from '@utils/cookie';
-
+import { Request, Response } from "express";
+import { validate } from "@middlewares/validate";
+import { asyncHandler } from "@middlewares/async";
+import { UserSchema } from "@schemas/user";
+import { issue } from "@utils/token";
+import { toResponse } from "@utils/response";
+import { cookie } from "@utils/cookie";
 
 export const authRouter = express.Router();
 
-
 authRouter.post(
-  "/", 
-  validate(UserSchema.auth, "body"), 
-  asyncHandler(async (req: Request, res: Response) => {
-    //creates a jwt
-    const token = issue(req.body)
-    //set cookie on client
-    res.cookie("auth", token, cookie);
-    res.status(200).send(toResponse("login success"));
-  }));
+	"/",
+	validate(UserSchema.auth, "body"),
+	asyncHandler(async (req: Request, res: Response) => {
+		//creates a jwt
+		const token = issue(req.body);
+		//set cookie on client
+		res.cookie("auth", token, cookie);
+		res.status(200).send(toResponse("login success"));
+	})
+);
