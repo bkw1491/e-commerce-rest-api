d373bfa92fdd088c18fd58a5546e38c766abcec3
Brandon <bkw1491@gmail.com>
2022-02-05 14:28:44 +0000

    clean up unused console.logs

--- models/Inventory.ts
+++ models/Inventory.ts
@@ -1,55 +1,45 @@
-import { Db } from '@config/database';
+import { Db } from "@config/database";
 import { IInventory } from "@interfaces/IInventory";
 
-
 export const InventoryModel = {
-
-  async findOne(product_id: number) {
-
-    const sql = `
+	async findOne(product_id: number) {
+		const sql = `
     
       SELECT * 
       FROM   inventory
       WHERE  product_id = $1`;
-    
-    const result = await Db.one<IInventory>(sql, [product_id]);
-    console.log(result);
-    return result;
-  },
 
-  
-  async createOne(inventory: Omit<IInventory, "id">) {
+		return await Db.one<IInventory>(sql, [product_id]);
+	},
 
-    const { product_id, quantity } = inventory
+	async createOne(inventory: Omit<IInventory, "id">) {
+		const { product_id, quantity } = inventory;
 
-    const sql = `
+		const sql = `
 
       INSERT INTO inventory (product_id, quantity)
       VALUES      ($1, $2)
       RETURNING   *`;
-  
-    return await Db.one<IInventory>(sql, [product_id, quantity]);
-  },
-   
 
-  async updateOne(inventory: Omit<IInventory, "id">) {
+		return await Db.one<IInventory>(sql, [product_id, quantity]);
+	},
 
-    const { product_id, quantity } = inventory
-  
-    const sql = `
+	async updateOne(inventory: Omit<IInventory, "id">) {
+		const { product_id, quantity } = inventory;
+
+		const sql = `
     
       UPDATE    inventory
       SET       quantity = $2 
       WHERE     product_id = $1
       RETURNING *`;
-  
-    return await Db.one<IInventory>(sql, [product_id, quantity]);
-  },
 
-  
-  async updateMany(user_id: number) {
-    //used to update the inventory once payment completes
-    const sql = `
+		return await Db.one<IInventory>(sql, [product_id, quantity]);
+	},
+
+	async updateMany(user_id: number) {
+		//used to update the inventory once payment completes
+		const sql = `
 
       UPDATE inventory AS i
       SET quantity = i.quantity - c.quantity
@@ -58,9 +48,8 @@ export const InventoryModel = {
         FROM cart
         WHERE user_id = $1
       ) AS c
-      WHERE i.product_id = c.product_id`
+      WHERE i.product_id = c.product_id`;
 
-    return await Db.many<IInventory>(sql, [user_id]);
-  }
-}
-
+		return await Db.many<IInventory>(sql, [user_id]);
+	}
+};
