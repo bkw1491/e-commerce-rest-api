adf3c2e60c00bbb40026e3246ef92edf8ca24a75
Brandon <bkw1491@gmail.com>
2022-02-06 09:46:09 +0000

    adjustments to req headers

--- middlewares/cors.ts
+++ middlewares/cors.ts
@@ -1,12 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 export function cors(req: Request, res: Response, next: NextFunction) {
-	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-	res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+	res.header("Access-Control-Allow-Origin", "http://*");
 	res.header("Access-Control-Allow-Credentials", "true");
 	res.header("Vary", "true");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested With, Content-Type, Accept"
-	);
 	next();
 }
