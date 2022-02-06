8ba48a45a53955391d228dabaec0db6fef365b88
Brandon <bkw1491@gmail.com>
2022-02-06 10:40:13 +0000

    add allow-methods headers

--- middlewares/cors.ts
+++ middlewares/cors.ts
@@ -1,10 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 export function cors(req: Request, res: Response, next: NextFunction) {
+	//need to specify the origin explicity since allow creds = true
 	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	//allow requests which contain these headers
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept"
 	);
+	//options is the pre-flight request
+	res.header(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, HEAD, OPTIONS"
+	);
+	//using cookies so need this
 	res.header("Access-Control-Allow-Credentials", "true");
 	next();
 }
