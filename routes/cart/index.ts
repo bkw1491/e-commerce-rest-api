d66754f1b3996cfb092e10c496409b6759e66f07
Brandon <bkw1491@gmail.com>
2022-02-06 11:18:36 +0000

    stripe payment url sent in res body rather than redirect

--- routes/cart/index.ts
+++ routes/cart/index.ts
@@ -1,76 +1,70 @@
-import express from 'express';
+import express from "express";
 
-import { Request, Response } from 'express';
-import { validate } from '@middlewares/validate';
-import { CartSchema } from '@schemas/cart';
-import { CartModel } from '@models/Cart';
-import { verifyJWT } from '@middlewares/verify';
-import { toResponse } from '@utils/response';
-import { asyncHandler } from '@middlewares/async';
-
+import { Request, Response } from "express";
+import { validate } from "@middlewares/validate";
+import { CartSchema } from "@schemas/cart";
+import { CartModel } from "@models/Cart";
+import { verifyJWT } from "@middlewares/verify";
+import { toResponse } from "@utils/response";
+import { asyncHandler } from "@middlewares/async";
 
 export const cartRouter = express.Router();
-
 
 //all routes here will require user priv
 cartRouter.use(verifyJWT("user"));
 
-
 //retrieving a user's cart
 cartRouter.get(
-  "/", 
-  validate(CartSchema.get, "body"),
-  asyncHandler(async (req: Request, res: Response) => {
+	"/",
+	validate(CartSchema.get, "body"),
+	asyncHandler(async (req: Request, res: Response) => {
+		const cart = await CartModel.findMany(req.body.user_id);
 
-    const cart = await CartModel.findMany(req.body.user_id);
-
-    res.status(200).send(toResponse(cart))
-  }));
-
+		res.status(200).send(toResponse(cart));
+	})
+);
 
 //checkout and pay
 cartRouter.post(
-  "/checkout", 
-  validate(CartSchema.checkout, "body"),
-  asyncHandler(async (req: Request, res: Response) => {
-
-    const paymentUrl = await CartModel.checkout(req.body.cart);
-    //303 = redirected, redirects to stripe hosted checkout
-    res.redirect(303, paymentUrl);
-  }));
-
+	"/checkout",
+	validate(CartSchema.checkout, "body"),
+	asyncHandler(async (req: Request, res: Response) => {
+		const paymentUrl = await CartModel.checkout(req.body.cart);
+		//run into lots of trouble with cors on front-ends when redirecting
+		//easier to send the link back to the client in res body
+		res.status(200).send(paymentUrl);
+	})
+);
 
 //add items to a cart
 cartRouter.post(
-  "/", 
-  validate(CartSchema.create, "body"),
-  asyncHandler(async (req: Request, res: Response) => {
-
-    const cart = await CartModel.createOne(req.body);
-    //201 = created
-    res.status(201).send(toResponse(cart));
-  }));
-
+	"/",
+	validate(CartSchema.create, "body"),
+	asyncHandler(async (req: Request, res: Response) => {
+		const cart = await CartModel.createOne(req.body);
+		//201 = created
+		res.status(201).send(toResponse(cart));
+	})
+);
 
 //update an item in a cart
 cartRouter.put(
-  "/",  
-  validate(CartSchema.update, "body"),
-  asyncHandler(async (req: Request, res: Response) => {
+	"/",
+	validate(CartSchema.update, "body"),
+	asyncHandler(async (req: Request, res: Response) => {
+		const cart = await CartModel.updateOne(req.body);
 
-    const cart = await CartModel.updateOne(req.body);
-
-    res.status(200).send(toResponse(cart));    
-  }));
-
+		res.status(200).send(toResponse(cart));
+	})
+);
 
 //remove items from a cart
 cartRouter.delete(
-  "/",  
-  validate(CartSchema.delete, "body"),
-  asyncHandler(async (req: Request, res: Response) => {
+	"/",
+	validate(CartSchema.delete, "body"),
+	asyncHandler(async (req: Request, res: Response) => {
+		const cart = await CartModel.deleteOne(req.body.id);
 
-    const cart = await CartModel.deleteOne(req.body.id);
-
-    res.status(200).send(toResponse(cart));    
-  }));
+		res.status(200).send(toResponse(cart));
+	})
+);
