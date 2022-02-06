486dec8c201408cc73c7396ae36c223643a44c5a
Brandon <bkw1491@gmail.com>
2022-02-06 10:13:18 +0000

    add allow headers

--- middlewares/cors.ts
+++ middlewares/cors.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 export function cors(req: Request, res: Response, next: NextFunction) {
 	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested With, Content-Type, Accept"
+	);
 	res.header("Access-Control-Allow-Credentials", "true");
 	next();
 }
